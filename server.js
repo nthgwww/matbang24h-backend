b748e1da9b52e768777a93fa3fa3e3d908103273
ngocthong <6051071118@st.utc2.edu.vn>
2023-05-07 15:19:34 +0700

    server

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express'
+require('dotenv').config()
+import cors from 'cors'
+import initRoutes from './src/routes'
+import connectDatabase from './src/config/connectDatabase'
+
+const app = express()   
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods:["POST","GET",'PUT','DELETE']
+}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+// app.use('/', (req,res)=>{res.send('server on ....')})
+initRoutes(app)
+connectDatabase()
+
+const port = process.env.PORT || 8888
+const listener = app.listen(port, ()=>{
+    console.log(`Server is running on port ${listener.address().port}`)
+})
